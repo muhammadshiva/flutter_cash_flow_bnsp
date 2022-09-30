d0c5518e6632cea8fe5e282e0d1abff13c6e4c40
muhammadshiva <shivamuhammad07@gmail.com>
2022-09-30 16:33:15 +0700

    fixing erros

--- lib/beranda_page.dart
+++ lib/beranda_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_cash_flow/login_page.dart';
 // import 'package:flutter_application_1/tambah_pemasukan.dart';
 
-
 class BerandaPage extends StatelessWidget {
   const BerandaPage({super.key});
 
@@ -14,121 +13,139 @@ class BerandaPage extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(title: Text("Rangkuman Bulan Ini")),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: 10),
-            Container( 
-              child: Text(
-                "Pengeluaran Rp. 500.000",
-                style: TextStyle(color: Colors.red, ),
-              ),
-            ),
-            Container(
-              child: Text(
-                "Pemasukan Rp. 1.000.000",
-                style: TextStyle(color: Colors.green, ),
-              ),
-            ),
-            Container(
-              width: 400,
-              height: 250,
-              child: Image(
-                  fit: BoxFit.fill,
-                  image: AssetImage("images/line-graph.png")),
-            ),
-            Expanded(
-              child:
-              GridView(
-                padding: EdgeInsets.all(10),
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 10,
-                  crossAxisSpacing: 10
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 10),
+              Container(
+                child: Text(
+                  "Pengeluaran Rp. 500.000",
+                  style: TextStyle(
+                    color: Colors.red,
+                  ),
                 ),
-                children: [
-                  Container(
-                    width: 5,
-                    height: 5,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 5,
-                      ),
-                    ),
-                    child: FloatingActionButton(onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context){
-                        return LoginPage();
-                      },));
-                    },
-                    heroTag: "btn1",
-                     backgroundColor: Colors.white,
-                     child: Image(image: AssetImage("images/add-income.png")),),
-                    // child: Image(image: AssetImage("images/add-income.png")),
-                  ),
-                  Container(
-                    width: 5,
-                    height: 5,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 5,
-                      ),
-                    ),
-                    child: FloatingActionButton(onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context){
-                        return LoginPage();
-                      },));
-                    },
-                    heroTag: "btn2",
-                     backgroundColor: Colors.white,
-                     child: Image(image: AssetImage("images/charity.png")),),
-                  ),
-                  Text("tambah Pemasukan"),
-                  Text("Tambah Pengeluaran"),
-                  Container(
-                    width: 5,
-                    height: 5,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 5,
-                      ),
-                    ),
-                    child: FloatingActionButton(onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context){
-                        return LoginPage();
-                      },));
-                    },
-                    heroTag: "btn3",
-                     backgroundColor: Colors.white,
-                     child: Image(image: AssetImage("images/add-document.png")),),
-                  ),
-                  Container(
-                    width: 5,
-                    height: 5,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 5,
-                      ),
-                    ),
-                    child: FloatingActionButton(onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context){
-                        return LoginPage();
-                      },));
-                    },
-                    heroTag: "btn4",
-                     backgroundColor: Colors.white,
-                     child: Image(image: AssetImage("images/settings.png")),),
-                  ),
-                  Text("Detail cash flow"),
-                  Text("Pengaturan"),
-                ],
               ),
-            ),
-            
-        ]),
+              Container(
+                child: Text(
+                  "Pemasukan Rp. 1.000.000",
+                  style: TextStyle(
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+              Container(
+                width: 400,
+                height: 250,
+                child: Image.asset(
+                  'assets/line-graph.png',
+                  fit: BoxFit.contain,
+                ),
+              ),
+              Expanded(
+                child: GridView(
+                  padding: EdgeInsets.all(10),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 10,
+                      crossAxisSpacing: 10),
+                  children: [
+                    Container(
+                      width: 5,
+                      height: 5,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 5,
+                        ),
+                      ),
+                      child: FloatingActionButton(
+                        onPressed: () {
+                          Navigator.push(context, MaterialPageRoute(
+                            builder: (context) {
+                              return LoginPage();
+                            },
+                          ));
+                        },
+                        heroTag: "btn1",
+                        backgroundColor: Colors.white,
+                        child: Image.asset('assets/add-income.png'),
+                      ),
+                      // child: Image(image: AssetImage("assets/add-income.png")),
+                    ),
+                    Container(
+                      width: 5,
+                      height: 5,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 5,
+                        ),
+                      ),
+                      child: FloatingActionButton(
+                        onPressed: () {
+                          Navigator.push(context, MaterialPageRoute(
+                            builder: (context) {
+                              return LoginPage();
+                            },
+                          ));
+                        },
+                        heroTag: "btn2",
+                        backgroundColor: Colors.white,
+                        child: Image.asset('assets/charity.png'),
+                      ),
+                    ),
+                    Text("tambah Pemasukan"),
+                    Text("Tambah Pengeluaran"),
+                    Container(
+                      width: 5,
+                      height: 5,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 5,
+                        ),
+                      ),
+                      child: FloatingActionButton(
+                        onPressed: () {
+                          Navigator.push(context, MaterialPageRoute(
+                            builder: (context) {
+                              return LoginPage();
+                            },
+                          ));
+                        },
+                        heroTag: "btn3",
+                        backgroundColor: Colors.white,
+                        child: Image.asset('assets/add-document.png'),
+                      ),
+                    ),
+                    Container(
+                      width: 5,
+                      height: 5,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 5,
+                        ),
+                      ),
+                      child: FloatingActionButton(
+                        onPressed: () {
+                          Navigator.push(context, MaterialPageRoute(
+                            builder: (context) {
+                              return LoginPage();
+                            },
+                          ));
+                        },
+                        heroTag: "btn4",
+                        backgroundColor: Colors.white,
+                        child: Image.asset('assets/settings.png'),
+                      ),
+                    ),
+                    Text("Detail cash flow"),
+                    Text("Pengaturan"),
+                  ],
+                ),
+              ),
+            ]),
       ),
     );
   }
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -7,77 +7,78 @@ import 'package:flutter_cash_flow/beranda_page.dart';
 const users = const {
   'fransiskaly@gmail.com': '12345678',
 };
-class LoginPage extends StatelessWidget {
-Duration get loginTime => Duration(milliseconds: 2250);
 
-Future<String> _authUser(LoginData data) {
-    print('Name: ${data.name}, Password: ${data.password}');
-    return Future.delayed(loginTime).then((_) {
-      if (!users.containsKey(data.name)) {
-        return 'Username not exists';
-      }
-      if (users[data.name] != data.password) {
-        return 'Password does not match';
-      }
-      return null;
-    });
-  }
-  
+class LoginPage extends StatelessWidget {
+  Duration get loginTime => Duration(milliseconds: 2250);
+
+// Future<String> _authUser(LoginData data) {
+//     print('Name: ${data.name}, Password: ${data.password}');
+//     return Future.delayed(loginTime).then((_) {
+//       if (!users.containsKey(data.name)) {
+//         return 'Username not exists';
+//       }
+//       if (users[data.name] != data.password) {
+//         return 'Password does not match';
+//       }
+//       return null;
+//     });
+//   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-              Container(
-                width: 200,
-                height: 200,
-                child: Image(
-                  fit: BoxFit.cover,
-                  image: AssetImage("images/cash-flow.png")),
+          body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            width: 200,
+            height: 200,
+            child: Image.asset(
+              'assets/cash-flow.png',
+              fit: BoxFit.contain,
+            ),
+          ),
+          Container(
+            child: Text(
+              "MyCashBook V1.0",
+              style: TextStyle(fontFamily: 'Libre', fontSize: 20),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: TextField(
+              autocorrect: false,
+              autofocus: true,
+              decoration: InputDecoration(
+                hintText: "Username",
               ),
-              Container(
-                child: Text(
-                  "MyCashBook V1.0",
-                  style: TextStyle(fontFamily: 'Libre', fontSize: 20),
-                  ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: TextField(
+              autocorrect: false,
+              autofocus: false,
+              decoration: InputDecoration(
+                hintText: "Password",
               ),
-              Padding(
-                padding: const EdgeInsets.all(20),
-                child: TextField(
-                  autocorrect: false,
-                  autofocus: true,
-                  decoration: InputDecoration(
-                    hintText: "Username",
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20),
-                child: TextField(
-                  autocorrect: false,
-                  autofocus: false,
-                  decoration: InputDecoration(
-                    hintText: "Password",
-                  ),
-                  
-                ),
-              ),
-              Container(
-                width: 320,
-                child: ElevatedButton(
-                onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context){
+            ),
+          ),
+          Container(
+            width: 320,
+            child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
                     return BerandaPage();
                   }));
-                }, 
+                },
                 child: const Text("Login")),
-              )
-              
-        ],)),
+          )
+        ],
+      )),
     );
   }
 }
